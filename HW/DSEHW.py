42a9e173cd1bce461e52b40f392ba9569e1933ed
Ronald Adomako <ronaldadomako@icloud.com>
2020-05-06 23:50:43 -0400

    fit learning algorithms to blobs WIP

--- HW/DSEHW.py
+++ HW/DSEHW.py
@@ -1,4 +1,18 @@
 #https://www.python.org/dev/peps/pep-0008/
 
-class DSELinearClassifier(activation):
-    return
+class DSELinearClassifier():
+    
+    def __init__(self,activation=None):
+        self.activation = activation
+        
+        if self.activation  == 'Perceptron':
+            print("Your activation is Perception")
+        elif self.activation == 'AdalineGD':
+            print("Your activation is AdalineGD")
+        elif self.activation == 'HyperTan':
+            print("Your activation is HyperTan")
+        elif self.activation not in ['Perceptron','AdalineGD','HyperTan']:
+            self.activation = 'Perceptron'
+            print("Your activation is defaulted to Perception. You can set the activation argument to 'AdalineGD' or 'HyperTan'")
+        
+        return
